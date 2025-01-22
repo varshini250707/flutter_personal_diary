754a51896696a15a99b03f1faa80018636858e21
varshini <varshu2530@gmail.com>
2025-01-22 12:29:16 +0530

    Create MessageScreen.dart

---
+++ lib/Screens/MessageScreen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MessageScreen extends StatefulWidget {
+
+
+  final String title;
+
+  const MessageScreen({super.key, required this.title});
+
+  @override
+  State<MessageScreen> createState() => _MessageScreenState();
+}
+
+class _MessageScreenState extends State<MessageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Colors.pinkAccent,
+      ),
+      body: Center(
+        child: Text(
+          widget.title
+        ),
+      ),
+    );
+  }
+}
