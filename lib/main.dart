39047a19a55719568ea705bb51ddeed24b19c6e0
varshini <varshu2530@gmail.com>
2025-01-22 11:38:09 +0530

    Home Screen app bar title changed

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(title: "Home",),
+      home: HomeScreen(title: "Personal Diary",),
     );
   }
 }
