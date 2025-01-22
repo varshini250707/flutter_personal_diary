b7d52d39f6975d30ef9a55c79f86c04681cbf754
varshini <varshu2530@gmail.com>
2025-01-22 11:38:29 +0530

    Home GridView UI added

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-
   final String title;
 
   const HomeScreen({super.key, required this.title});
@@ -11,6 +10,14 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final List<String> items = [
+    'Contacts',
+    'Messages',
+    'Pepole',
+    'Battery',
+    'Analyze Surrounding'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,10 +25,26 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text(widget.title),
         backgroundColor: Colors.amber,
       ),
-      body: Center(
-        child: Text(
-          widget.title,
-          style: TextStyle(fontSize: 24),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 10.0,
+            mainAxisSpacing: 10.0,
+          ),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return Card(
+              color: Colors.blueAccent,
+              child: Center(
+                child: Text(
+                  items[index],
+                  style: const TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
