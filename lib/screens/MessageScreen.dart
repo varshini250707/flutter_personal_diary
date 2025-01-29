29fb127be89a8ef856265d1badda3ba122ba0261
varshini250707 <varshu2530@gmail.com>
2025-01-29 14:59:18 +0530

    MessageScreen  ui added

--- lib/screens/MessageScreen.dart
+++ lib/screens/MessageScreen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class MessageScreen extends StatefulWidget {
-
-
   final String title;
 
   const MessageScreen({super.key, required this.title});
@@ -12,6 +10,14 @@ class MessageScreen extends StatefulWidget {
 }
 
 class _MessageScreenState extends State<MessageScreen> {
+  final List<Map<String, String>> messages = [
+    {'phone': '123-456-7890', 'message': 'Hello! How are you?'},
+    {'phone': '987-654-3210', 'message': 'Meeting at 3 PM today.'},
+    {'phone': '555-666-7777', 'message': 'Don’t forget the groceries!'},
+    {'phone': '444-333-2222', 'message': 'Happy Birthday!'},
+    {'phone': '111-222-3333', 'message': 'Call me when you are free.'},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,10 +25,18 @@ class _MessageScreenState extends State<MessageScreen> {
         title: Text(widget.title),
         backgroundColor: Colors.blueAccent,
       ),
-      body: Center(
-        child: Text(
-          widget.title
-        ),
+      body: ListView.builder(
+        itemCount: messages.length,
+        itemBuilder: (context, index) {
+          return Card(
+            margin: EdgeInsets.all(10),
+            child: ListTile(
+              leading: Icon(Icons.phone, color: Colors.blueAccent),
+              title: Text(messages[index]['phone']!),
+              subtitle: Text(messages[index]['message']!),
+            ),
+          );
+        },
       ),
     );
   }
