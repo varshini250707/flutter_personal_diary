6666adfe83f820738ca1aa5c0fc975d1264e0cc4
varshini250707 <varshu2530@gmail.com>
2025-02-05 15:17:26 +0530

    MobileContactScreen

--- lib/screens/contact/MobileContactScreen.dart
+++ lib/screens/contact/MobileContactScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
 
 class MobileContactScreen extends StatefulWidget {
 
@@ -11,13 +12,48 @@ class MobileContactScreen extends StatefulWidget {
 }
 
 class _MobileContactScreenState extends State<MobileContactScreen> {
+
+  List<Contact> _contacts = [];
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchContacts();
+  }
+
+  Future<void> _fetchContacts() async {
+    if (await FlutterContacts.requestPermission()) {
+      List<Contact> contacts = await FlutterContacts.getContacts(withProperties: true);
+      setState(() {
+        _contacts = contacts;
+        _isLoading = false;
+      });
+    } else {
+      setState(() => _isLoading = false);
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(widget.title),
+      // appBar: AppBar(title: Text('Contacts')),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : _contacts.isEmpty
+          ? Center(child: Text('No contacts found'))
+          : ListView.builder(
+        itemCount: _contacts.length,
+        itemBuilder: (context, index) {
+          final contact = _contacts[index];
+          return ListTile(
+            leading: CircleAvatar(
+              child: Text(contact.displayName[0]),
+            ),
+            title: Text(contact.displayName),
+            subtitle: Text(contact.phones.isNotEmpty ? contact.phones.first.number : 'No number'),
+          );
+        },
       ),
     );
   }
 }
-
